55b10039f2930632cd67e1aa7334efdbce07d2ce
Dobbs, Brendan <brendan.dobbs@sap.com>
2019-07-03 12:17:34 +0100

    Fix for GDM redirect issue https://github.com/SAP/xf-addons/issues/12. The API URLs need to have a '/' on the end of them otherwise a redirect is issued (307) which the go http client is not following correctly (500 error).

--- api-registration-job/main.go
+++ api-registration-job/main.go
@@ -186,7 +186,7 @@ func (r registrationApp) getRegisteredAPIs() []API {
 }
 
 func (r registrationApp) isAPIActive(path string) (bool, error) {
-	url := r.SystemURL + "/" + path
+	url := r.SystemURL + "/" + path + "/"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return false, err
